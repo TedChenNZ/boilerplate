cd7ab65139ebe229b1e3894cee6554deb199140c
Ted Chen <tedchennz@gmail.com>
2018-08-08 10:25:11 +1200

    Add mobx

---
+++ src/store/appStore.tsx
@@ -0,0 +1,7 @@
+import { observable, action, configure, runInAction } from 'mobx';
+configure({ enforceActions: true });
+
+
+export default class AppStore {
+
+}
---
+++ src/store/configureMobxStore.ts
@@ -0,0 +1,15 @@
+import AppStore from "./appStore";
+
+
+export default function configureMobxStore() {
+
+  const appStore = new AppStore();
+
+  const store = {
+    appStore,
+  };
+
+  // Initialise
+
+  return store;
+}
