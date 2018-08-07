ff5620f24ab22f472b903d3cd396e406eb155fe8
Ted Chen <tedchennz@gmail.com>
2018-08-08 10:58:06 +1200

    Cleanup

--- src/store/appStore.tsx
+++ src/store/appStore.tsx
@@ -2,5 +2,11 @@ import { action, configure, observable, runInAction } from 'mobx';
 configure({ enforceActions: true });
 
 export default class AppStore {
+  @observable loading = true;
+
+  @action
+  setLoading = (loading) => {
+    this.loading = loading;
+  }
 
 }
